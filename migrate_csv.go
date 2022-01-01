8943f4d23d1a932dbbcd96452e75d5811d2840a1
Suraj Bobade <iamsurajbobade@gmail.com>
2022-01-01 13:12:53 +0530

    `Write` to *.csv file

--- migrate_csv.go
+++ migrate_csv.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"encoding/csv"
-	"fmt"
 	"io"
 	"log"
 	"os"
 )
 
 var (
-	fileName string = "logins.csv"
+	fileName    string = "logins.csv"
+	outFileName string = "logins_processed.csv"
 )
 
 func main() {
@@ -25,6 +25,18 @@ func main() {
 		log.Fatal("Error reading file", err)
 	}
 
+	outputFile, err := os.Create(outFileName)
+	if err != nil {
+		log.Fatal("Could not open file", err)
+	}
+	defer outputFile.Close()
+	writer := csv.NewWriter(outputFile)
+	defer writer.Flush()
+
+	if err = writer.Write([]string{"url", "username", "password"}); err != nil {
+		log.Fatal("Could not write to output file", err)
+	}
+
 	for {
 		line, err := reader.Read()
 
@@ -34,6 +46,7 @@ func main() {
 		if err != nil {
 			log.Fatal("Could not process file entry", err)
 		}
-		fmt.Println(line)
+		//fmt.Println(line)
+		err = writer.Write([]string{line[0], line[1], line[2]})
 	}
 }
